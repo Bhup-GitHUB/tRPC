a31b29771828047cd805ea92f2f3000cb2137368
Bhupesh Kumar <bkumar_be23@thapar.edu>
2025-06-01 01:54:30 +0530

    feat(todos): implement createTodo procedure with Zod validation

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,22 @@
-import { router } from "./trpc";
+import { publicProcedure, router } from "./trpc";
+import { z } from "zod";
 
+const todoInputs = z.object({
+  title: z.string(),
+  description: z.string().optional(),
+});
 
-const appRouter = router({});
+const appRouter = router({
+  createTodo: publicProcedure.input(todoInputs).mutation(async (opts) => {
+    const title = opts.input.title;
+    const description = opts.input.description;
+
+    //db stuff here
+
+    return {
+      id: "1",
+    };
+  }),
+});
 
 export type AppRouter = typeof appRouter;
